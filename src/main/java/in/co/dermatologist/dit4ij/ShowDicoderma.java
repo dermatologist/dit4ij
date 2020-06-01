ae0a1ca7829e58ff66667ba8095e64c7a8c5f744
beapen <github@gulfdoctor.net>
2020-06-01 15:18:10 -0400

    start scaffold

---
+++ src/main/java/in/co/dermatologist/dit4ij/ShowDicoderma.java
@@ -0,0 +1,17 @@
+package in.co.dermatologist.dit4ij;
+
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+import org.scijava.plugin.Parameter;
+import net.imglib2.type.numeric.RealType;
+import org.scijava.command.Command;
+
+@Plugin(type = Command.class, menuPath = "Plugins>Dicoderma>Show")
+public class ShowDicoderma implements Command {
+
+    @Override
+    public void run() {
+
+    }
+
+}
